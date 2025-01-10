7fca12b2cf7737f781750eca97f105794c843692
picasso09 <119201284+picasso09@users.noreply.github.com>
2025-01-10 19:04:00 +0700

    sign: setup device signed
    Signed-off-by: picasso09 <119201284+picasso09@users.noreply.github.com>
    
    Date:      Fri Jan 10 19:04:00 2025 +0700
    Change-Id: 7cfda13a814af817e969f2babe27e987d9c1e655

--- make_key.sh
+++ make_key.sh
@@ -6,4 +6,4 @@
 set -u
 bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
     $1 \
-    '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+    '/C=ID/ST=Indonesian/L=Sumatera View/O=DV/OU=DiscussionVerse/CN=picasso09/emailAddress=picasso09@afterlifeteam.com'
